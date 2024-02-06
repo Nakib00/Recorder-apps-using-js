6ebe10b60deb7a8e430e5a6dffb99ea2326703da
Nakib00 <2022142@iub.edu.bd>
2024-02-07 05:47:47 +0600

    update data

--- js/dataset.js
+++ js/dataset.js
@@ -1,28 +1,28 @@
 // JavaScript code to change table body based on dropdown selection
 document.getElementById("option1").addEventListener("click", function () {
-    changeTableBody("MYMENSINGH Division", ["যাবা", "বুজেছ", "সে", "হ্যাঁ", "হবে"]);
+    changeTableBody("MYMENSINGH Division", ["ঠেহাল, আইনহে, আমগোর, আহুইন, তুইল্লা, এমনে, এইতা, আইসুইন, এমতে, এডা, ও কেডা, কুইনে, কইরালা, কেমতে, কইরালছি, কিতা, খাওন, গাতা, হইসে, ছোডো, হুনহাইন, থাহুইন, নিয়া, নয়া, হাল, পোলা, বইলা, আবু, বুইত্তা, ডর, বিত্তে, চালুন, এরপর, ঢুহানো, ভালাই, হেম, ব্যাডা, বেডি, মইরা গেছে, মাইয়া, হুইতে, হাছা, মিছা, যাইবা, বুজচইন"]);
 });
 
   document.getElementById("option2").addEventListener("click", function () {
-    changeTableBody("DHAKA Division",["যাবা", "বুজেছ", "সে", "হ্যাঁ", "হবে"]);
+    changeTableBody("DHAKA Division",["আহৈন, কেমতে, খায়োন, অইব"]);
   });
   document.getElementById("option3").addEventListener("click", function () {
-    changeTableBody("RAJSHAHI Division",["যাবা", "বুজেছ", "সে", "হ্যাঁ", "হবে"]);
+    changeTableBody("RAJSHAHI Division",["আমাক, আপনেক, আমারেক, আ্যাসেন, মেলা, তুলা, এই-ক্যাোরি, আ্যাসিছেন, ঐ-ক্যোরি, এডি, ওক, কোতি, কি-ক্যাোরি, করিছি, কি-বে, খ্যা, গা-ধোয়া, হ্যোচে, চ্যুলি, ছুটু, শুনেক, আখাড়-পাড়, থ্যাকতে, থ্যাকি, লিজের, নি, লতুন, মামুর ব্যাটা / চ্যাংড়া, বুলা, ছুটু ছাওয়াল, পেম, মিয়া, ম্যোরি-গ্যাছে, শ্যুতে, সত্যি, মুজাক"]);
   });
   document.getElementById("option4").addEventListener("click", function () {
-    changeTableBody("RANGPUR Division");
+    changeTableBody("RANGPUR Division", ["মংগা,হামি,হামরা,হামার,কোনঠে,ছোটোকোনা,আন্দঘর, চ্যাংড়া, ছাওয়া"]);
   });
   document.getElementById("option5").addEventListener("click", function () {
-    changeTableBody("SYLHET Division",["যাবা", "বুজেছ", "সে", "হ্যাঁ", "হবে"]);
+    changeTableBody("SYLHET Division",["আও, ইলাখান, ওইলাখান, ওটা, ওগু, কূনানো, কি লাখান, খরাপ, উন্দাল, হোরো, হোনো, ফাকঘর, নে,নয়া, ফুয়া, খও,হুরুতা,পীড়ীত, মরিগেছে, ফুরি,হাছা"]);
   });
   document.getElementById("option6").addEventListener("click", function () {
-    changeTableBody("CHITTAGONG Division",["যাবা", "বুজেছ", "সে", "হ্যাঁ", "হবে"]);
+    changeTableBody("CHITTAGONG Division",["অবাব, আঁই, অনে, আঁরা, আঁরার, আইয়্যুন, আরো, উডোই রাহো, এন্ডেইল্লে, এগিন, আইস্যি, ইয়েন, ইতেরে, হন্ডে, গরো, হইল্লে, গইজ্জি, হন্, হারাপ, গোসল, গাঁত, অইয়্যি, আসি, ছুলো, ছুডো, উনো, বোসহানা, থাইকতে, থাগি, নিজুর, নাই, লন্, নোয়া, ফোয়া, ফানি, হওয়া, ফোয়া-ছা, ডঁর, গম, ভুতুরে, saloin, তারফর, সেরকম, ফ্রেম, মরত ফোয়্যা, মাঁইফুয়া, মরি গেইয়্যি, মাইফুয়া, আছা, মিছে, যাইবে, বুজ্জুউইস না, এতে, ওও, হইব, আর, আন্ডা, আইয়েন, উডানো, এগিন, আইছে, হেন্নে,ইয়েন, হেতেরে, কোনানে, কেন্নে, কইচ্ছি, খাওন, খাদ, অইছে, আসি, ছোড, হুনো,হাকঘরে, থাইকতো, নেয়, হোলা, হানি, কওয়া, গূয়া হোলা, বড্ডা, বালা, হেতে, ভিত্তে,এ য়ার-হরে, হান্দাউ, পিরিত"]);
   });
   document.getElementById("option7").addEventListener("click", function () {
-    changeTableBody("BARISAL Division",["যাবা", "বুজেছ", "সে", "হ্যাঁ", "হবে"]);
+    changeTableBody("BARISAL Division",["ঠ্যক, মুই, আমনে, বেটটি, আমাগো, আয়েন, আরও, উচ্কান, এডই, আইসোস, লইয়ে, হেই, কও, ভালা, ডর, হেরপর, ডক, হাঁচা, হে, হ"]);
   });
   document.getElementById("option8").addEventListener("click", function () {
-    changeTableBody("KHULNA Division",["যাবা", "বুজেছ", "সে", "হ্যাঁ", "হবে"]);
+    changeTableBody("KHULNA Division",["আসেন, উডাউ, ওভাবে, এগুলা, আসিছিলেন, ওভাবে, ওইটা, কনতে, করতে লাগো, কেম্নে , করিছি, কিতা, ভেটকা, থাকতি, নাই, নিয়ে, তাপপর, মরিগেছে, শুতি, বুজিসিশ"]);
   });
 
   function changeTableBody(content, data) {
